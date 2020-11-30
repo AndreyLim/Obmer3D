376b8f97b13a92ec32e391b58d31653cbdb66e39
AndreyLim <68769335+AndreyLim@users.noreply.github.com>
2020-11-30 19:44:14 +0300

    create: zakazphone & adaptive

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ autoplay: {
 $(document).ready(function(){
   $('.third-block__input--phone').mask('+7 (999) 999-99-99');
 });
+
+
+//кнопка вызова формы
+var b = document.getElementById('overlay');
+function swa(){
+	b.style.visibility = 'visible';
+	b.style.opacity = '1';
+	b.style.transition = 'all 0.7s ease-out 0s';
+}
+function swa2(){
+	b.style.visibility = 'hidden';
+	b.style.opacity = '0';
+}
